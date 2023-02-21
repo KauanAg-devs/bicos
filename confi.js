4d5e7ff90f4dcc58ae319d3c71a329a0be1da730
Cauezins <109925592+Cauezins@users.noreply.github.com>
2023-02-21 01:37:38 -0300

    Add files via upload

---
+++ confi.js
@@ -0,0 +1,8 @@
+function searchColor() {
+    document.getElementById('searchButton').style.backgroundColor = '#f5f5f5'
+    document.getElementById('search').style.backgroundColor = '#f5f5f5'
+}
+function searchColorNorm() {
+    document.getElementById('searchButton').style.backgroundColor = 'white'
+    document.getElementById('search').style.backgroundColor = 'white'
+}
